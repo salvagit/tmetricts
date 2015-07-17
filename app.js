f9854cbcf379d343716b76623cafa7389988024e
cesar casas <lortmorris@cesars-MacBook-Air.local>
2015-07-16 21:01:02 -0300

    leg

--- app.js
+++ app.js
@@ -33,6 +33,8 @@ app.use(express.static(__dirname + '/public'));
 app.use(function(req, res, next){
 
     res.set('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Allow-Methods', '*');
+    res.set('Access-Control-Allow-Headers', '*');
 
    res.out =  function(err, docs){
        if(err){ res.json({err: err, data: null});}
@@ -82,6 +84,46 @@ app.get("/hits/key/:key", function(req, res, next){
         res.out(err, docs);
     });
 });
+
+
+app.post("/comment", function(req, res, next){
+
+        var body = JSON.parse(Object.keys(req.body)[0]);
+
+        var params = {
+            name: body.name || "noName"
+            ,email : body.email || "NoMail"
+            ,message: body.message || ""
+            ,added :  new Date()
+
+        };
+
+    db.comments.save(params, function(err, d){
+        if(err){
+            res.out(err, null);
+            console.log("Error guardando el mensaje");
+        }
+        else{
+            console.log("Mensaje guardado ");
+            res.out(null, params);
+        }
+    })
+});
+
+
+app.get("/getcomments", function(req, res, next){
+
+
+    db.comments.find({}, {}).sort({added:-1}).limit(20).toArray( function(err, data){
+        if(err){
+            res.out(err, null);
+        }
+        else{
+            res.out(null, data);
+        }
+    })
+});
+
 
 
 var server = app.listen(port, function () {
